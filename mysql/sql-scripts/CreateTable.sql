1b2ecf58c8fda340d1b3560c5e6d78322b5a1cd7
Yohann Capel <yohann@MacBook-Air-de-Yohann.local>
2019-01-17 21:26:26 +0100

    yes

---
+++ mysql/sql-scripts/CreateTable.sql
@@ -0,0 +1,5 @@
+CREATE TABLE employees (
+id INT,
+tittle varchar(40),
+context TEXT,
+);
---
+++ mysql/sql-scripts/InsertData.sql
@@ -0,0 +1,2 @@
+INSERT INTO article (id,title,content) VALUES(1, “My First Article”,”This is a text 1”);
+INSERT INTO article (id,title,content) VALUES(2, “My Second Article”,”This is a text 2”);
